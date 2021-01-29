78473b7f8197ec760da0a51be5eb3a99ef89a834
arjunevolvan <arjunk@evolvan.com>
2021-01-29 17:33:54 +0530

    angular basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'youtube';
+  name = 'john smith';
+  currentVal = '';
+  getName(name:string){
+   alert(name)
+  }
+  getEvent(evt:string){
+    console.warn(evt)
+  }
+  getVal(evt:string){
+    return this.currentVal = evt;
+  }
+  disabledBox = true;
+  enableBox(){
+    this.disabledBox = false;
+
+  }
+  
+  obj ={
+    name:'anil',
+    age:20
+  }
+  arr = ['peter','smith','root'];
+
+  a = 100;
+  b = 200;
+
+  siteUrl = window.location.href;
 }
